3b0afd6520ed3ee54cd2df99cd8e25fb5aeaf3b9
Dante <db470@student.le.ac.uk>
2022-02-15 01:31:42 +0000

    added firebase check on login page

--- files/db470/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/LoginActivity.java
+++ files/db470/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidlogin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,11 +9,21 @@ import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     private EditText inputEmail, inputPassword;
     private Button btnLogin;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,47 +35,61 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnLogin:
-                if (login() == false) {
+                Login();
+                break;
+                /*if (login() == false) {
                     Intent Intent = new Intent(this, MainActivity.class);
                     startActivity(Intent);
-                }
+                }*/
         }
     }
-    private boolean login() {
+    private void Login() {
         String inputemail = inputEmail.getText().toString().trim();
         String inputpassword = inputPassword.getText().toString().trim();
 
         if (inputemail.isEmpty()) {
             inputEmail.setError("Email is required");
             inputEmail.requestFocus();
-            return true;
+            return;
         }
 
         if (!Patterns.EMAIL_ADDRESS.matcher(inputemail).matches()){
             inputEmail.setError("please provide a valid email address");
             inputEmail.requestFocus();
-            return true;
+            return;
         }
 
         if (inputpassword.isEmpty()) {
             inputPassword.setError("Password is required");
             inputPassword.requestFocus();
-            return true;
+            return;
         }
 
         if (inputpassword.length() < 6) {
             inputPassword.setError("Password should be at least 6 characters");
             inputPassword.requestFocus();
-            return true;
-        }else{
-            return false;
+            return;
         }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(inputemail, inputpassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }else{
+                    Toast.makeText(LoginActivity.this,"Failed to login! Please check your credentials!", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     //on click go to main page
