9a0320147b4073203e9eddbd866d3c9513730e0b
RuHostile <djb3501@gmail.com>
2022-02-19 16:11:36 +0000

    pushing all changes to firebaseAdapterSearch to branch

--- files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/SelectPatient.java
+++ files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/SelectPatient.java
@@ -2,36 +2,45 @@ package com.example.androidlogin;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
 
 public class SelectPatient extends AppCompatActivity {
 
     private EditText searchPatient, mSearchField;
     private Button searchButton, mSearchBtn;
-    private RecyclerView resultList;
+    private RecyclerView mResultList;
 
-    private DatabaseReference mPatientsDatabase;
+    private DatabaseReference mPatientDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_patient);
 
-        mPatientsDatabase = FirebaseDatabase.getInstance().getReference("Patients");
+        mPatientDatabase = FirebaseDatabase.getInstance().getReference("Patients");
 
         searchPatient = (EditText) findViewById(R.id.editTextSearchPatient);
         searchButton = (Button) findViewById(R.id.buttonSearch);
-        resultList = (RecyclerView) findViewById(R.id.recyclerViewResultList);
+        mResultList= (RecyclerView) findViewById(R.id.recyclerViewResultList);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
 
         mSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,21 +55,49 @@ public class SelectPatient extends AppCompatActivity {
                 Patients.class,
                 R.layout.list_layout,
                 PatientsViewHolder.class,
-                mPatientsDatabase
+                mPatientDatabase
         ) {
             @Override
-            protected void pupulateViewHolder(PatientsViewHolder viewHolder, Patients model, int position){
+            protected void onBindViewHolder(@NonNull PatientsViewHolder holder, int position, @NonNull Patients model) {
+                holder.setDetails(getApplicationContext(), model.getName(), model.getPicture());
+            }
 
-                viewHolder.setDetails(model.getName(), model.getPicture());
-
+            @NonNull
+            @Override
+            public PatientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
             }
         };
+        mResultList.setAdapter(firebaseRecyclerAdapter);
     }
+
+    /*
+    *     private void firebasePatientSearch() {
+        FirebaseRecyclerAdapter<Patients, PatientsViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Patients, PatientsViewHolder>(
+                Patients.class,
+                R.layout.list_layout,
+                PatientsViewHolder.class,
+                mPatientDatabase
+        ) {
+            @Override
+            protected void onBindViewHolder(@NonNull PatientsViewHolder holder, int position, @NonNull Patients model) {
+
+                holder.setDetails(getApplicationContext(), model.getName(), model.getPicture());
+            }
+
+            @NonNull
+            @Override
+            public PatientsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+        };
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+    }*/
 
 
     // View Holder Class
 
-    public class PatientsViewHolder extends RecyclerView.ViewHolder {
+    public static class PatientsViewHolder extends RecyclerView.ViewHolder {
 
         View mView;
 
@@ -70,11 +107,13 @@ public class SelectPatient extends AppCompatActivity {
             mView = itemView;
         }
 
-        public void setDetails(String patientName, String patientPicture){
+        public void setDetails(Context ctx, String patientName, String patientPicture){
             TextView patient_name = (TextView) mView.findViewById(R.id.textViewName);
             ImageView patient_picture = (ImageView) mView.findViewById(R.id.imageViewPicture);
 
             patient_name.setText(patientName);
+
+            Glide.with(ctx).load(patientPicture).into(patient_picture);
 
         }
 
