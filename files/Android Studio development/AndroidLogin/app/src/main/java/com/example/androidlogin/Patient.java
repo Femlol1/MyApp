6848bbc5be0a79fc36a14154f599d2ea5b55d43e
Dante <db470@student.le.ac.uk>
2022-02-22 02:24:16 +0000

    Changes to the patient_view.xml presentation

--- files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/Patient.java
+++ files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/Patient.java
@@ -3,7 +3,13 @@ package com.example.androidlogin;
 //This class will mimic the variable naming on the firebase data bases
 public class Patient {
 
-    public String name, dob, gender;
+    public String name, dob, gender, address, ambientTemperature,
+            clan, communicableDiseaseContact, diseaseVectors,
+            endemicDiseaseRegion, hospital, idAndNo, longTermEnvironmentExposure,
+            maritalStatus, nextOfKin, nextOfKinAddress, occupationalHistory,
+            professionalTraining, race, recentSanitationSystem, recentSourceOfFood,
+            recentSourceOfWater, regDate, relativeHumidity, totem, trainingHistory,
+            tribe,weatherTransition;
 
     public String getName() {
         return name;
@@ -15,6 +21,102 @@ public class Patient {
 
     public String getGender() {
         return gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getAmbientTemperature() {
+        return ambientTemperature;
+    }
+
+    public String getClan() {
+        return clan;
+    }
+
+    public String getCommunicableDiseaseContact() {
+        return communicableDiseaseContact;
+    }
+
+    public String getDiseaseVectors() {
+        return diseaseVectors;
+    }
+
+    public String getEndemicDiseaseRegion() {
+        return endemicDiseaseRegion;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public String getIdAndNo() {
+        return idAndNo;
+    }
+
+    public String getLongTermEnvironmentExposure() {
+        return longTermEnvironmentExposure;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getNextOfKin() {
+        return nextOfKin;
+    }
+
+    public String getNextOfKinAddress() {
+        return nextOfKinAddress;
+    }
+
+    public String getOccupationalHistory() {
+        return occupationalHistory;
+    }
+
+    public String getProfessionalTraining() {
+        return professionalTraining;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public String getRecentSanitationSystem() {
+        return recentSanitationSystem;
+    }
+
+    public String getRecentSourceOfFood() {
+        return recentSourceOfFood;
+    }
+
+    public String getRecentSourceOfWater() {
+        return recentSourceOfWater;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public String getRelativeHumidity() {
+        return relativeHumidity;
+    }
+
+    public String getTotem() {
+        return totem;
+    }
+
+    public String getTrainingHistory() {
+        return trainingHistory;
+    }
+
+    public String getTribe() {
+        return tribe;
+    }
+
+    public String getWeatherTransition() {
+        return weatherTransition;
     }
 }
 
--- files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/PatientAdapter.java
+++ files/Android Studio development/AndroidLogin/app/src/main/java/com/example/androidlogin/PatientAdapter.java
@@ -34,6 +34,30 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientV
         holder.name.setText(patient.getName());
         holder.dob.setText(patient.getDob());
         holder.gender.setText(patient.getGender());
+        holder.tv4.setText(patient.getAddress());
+        holder.tv5.setText(patient.getRace());
+        holder.tv6.setText(patient.getOccupationalHistory());
+        holder.tv7.setText(patient.getHospital());
+        holder.tv8.setText(patient.getIdAndNo());
+        holder.tv9.setText(patient.getNextOfKin());
+        holder.tv10.setText(patient.getNextOfKinAddress());
+        holder.tv11.setText(patient.getProfessionalTraining());
+        holder.tv12.setText(patient.getRecentSourceOfFood());
+        holder.tv13.setText(patient.getRecentSourceOfWater());
+        holder.tv14.setText(patient.getRecentSanitationSystem());
+        holder.tv15.setText(patient.getRegDate());
+        holder.tv16.setText(patient.getTrainingHistory());
+        holder.tv17.setText(patient.getClan());
+        holder.tv18.setText(patient.getTotem());
+        holder.tv19.setText(patient.getTribe());
+        holder.tv20.setText(patient.getMaritalStatus());
+        holder.tv21.setText(patient.getWeatherTransition());
+        holder.tv22.setText(patient.getLongTermEnvironmentExposure());
+        holder.tv23.setText(patient.getEndemicDiseaseRegion());
+        holder.tv24.setText(patient.getDiseaseVectors());
+        holder.tv25.setText(patient.getCommunicableDiseaseContact());
+        holder.tv26.setText(patient.getAmbientTemperature());
+        holder.tv27.setText(patient.getRelativeHumidity());
     }
 
     @Override
@@ -43,13 +67,39 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientV
 
     public static class PatientViewHolder extends RecyclerView.ViewHolder{
 
-        TextView name, dob, gender;
+        TextView name, dob, gender, tv4, tv5, tv6, tv7, tv8, tv9, tv10, tv11,
+                tv12, tv13, tv14, tv15, tv16, tv17, tv18, tv19, tv20, tv21,
+                tv22, tv23, tv24, tv25, tv26, tv27;
 
         public PatientViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.textViewName);
             dob = itemView.findViewById(R.id.textViewDOB);
             gender = itemView.findViewById(R.id.textViewGender);
+            tv4 = itemView.findViewById(R.id.textView4);
+            tv5 = itemView.findViewById(R.id.textView5);
+            tv6 = itemView.findViewById(R.id.textView6);
+            tv7 = itemView.findViewById(R.id.textView7);
+            tv8 = itemView.findViewById(R.id.textView8);
+            tv9 = itemView.findViewById(R.id.textView9);
+            tv10 = itemView.findViewById(R.id.textView10);
+            tv11 = itemView.findViewById(R.id.textView11);
+            tv12 = itemView.findViewById(R.id.textView12);
+            tv13 = itemView.findViewById(R.id.textView13);
+            tv14 = itemView.findViewById(R.id.textView14);
+            tv15 = itemView.findViewById(R.id.textView15);
+            tv16 = itemView.findViewById(R.id.textView16);
+            tv17 = itemView.findViewById(R.id.textView17);
+            tv18 = itemView.findViewById(R.id.textView18);
+            tv19 = itemView.findViewById(R.id.textView19);
+            tv20 = itemView.findViewById(R.id.textView20);
+            tv21 = itemView.findViewById(R.id.textView21);
+            tv22 = itemView.findViewById(R.id.textView22);
+            tv23 = itemView.findViewById(R.id.textView23);
+            tv24 = itemView.findViewById(R.id.textView24);
+            tv25 = itemView.findViewById(R.id.textView25);
+            tv26 = itemView.findViewById(R.id.textView26);
+            tv27 = itemView.findViewById(R.id.textView27);
         }
     }
 }
